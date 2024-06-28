f02948a69ce826d22d0dfb819717ca743ae44722
Jae Won Choi <djc0105@gmail.com>
2024-06-28 11:23:58 +0900

    upate

--- waitress_server.py
+++ waitress_server.py
@@ -1,6 +1,15 @@
+import argparse
 from waitress import serve
 from boneage import create_app
         
 if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Run Bone Age Server.")
+    parser.add_argument('--host', type=str, default='0.0.0.0', help='DONT TOUCH')
+    parser.add_argument('--port', type=int, default=19001, help='server port')
+    args = parser.parse_args()
+    
+    host = args.host
+    port = args.port
+    
     app = create_app()
-    serve(app, host='0.0.0.0', port=19001)
+    serve(app, host=host, port=port)
